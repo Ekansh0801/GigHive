5bcb95885fefccd725a1979033020fd8759d161b
Ekansh0801 <ekanshaggarwal11103@gmail.com>
2024-05-26 11:06:02 +0530

    Worked on Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,72 @@
 import Navbar from "./components/Navbar";
-
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Gigs from "./pages/Gigs";
+import Gig from "./pages/Gig";
+import Orders from "./pages/Orders";
+import MyGigs from "./pages/MyGigs";
+import Add from "./pages/Add";
+import Messages from "./pages/Messages";
+import Message from "./pages/Message";
 const App = () => {
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar/>
+        <Outlet/>
+        <Footer/>
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/gigs",
+          element:<Gigs/>
+        },
+        {
+          path:"/gig/:id",
+          element:<Gig/>
+        },
+        {
+          path:"/orders",
+          element:<Orders/>
+        },
+        {
+          path:"/mygigs",
+          element:<MyGigs/>
+        },
+        {
+          path:"/gigs",
+          element:<Gigs/>
+        },
+        {
+          path:"/add",
+          element:<Add/>
+        },
+        {
+          path:"/messages",
+          element:<Messages/>
+        },
+        {
+          path:"/message/:id",
+          element:<Message/>
+        },
+      ]
+    }
+  ])
   return (
     <div>
-      <Navbar/>
+      <RouterProvider router={router}/>
     </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Featured from '../components/Featured'
+import TrustedBy from '../components/TrustedBy'
+
+const Home = () => {
+  return (
+    <div>
+        <Featured/>
+        <TrustedBy/>
+    </div>
+  )
+}
+
+export default Home
