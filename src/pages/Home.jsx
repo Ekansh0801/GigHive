ad69442b27598580972c4c80440ed18896f60a90
Ekansh0801 <ekanshaggarwal11103@gmail.com>
2024-05-29 18:26:03 +0530

    Infinite slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Featured from '../components/Featured'
 import TrustedBy from '../components/TrustedBy'
+import Slide from '../components/Slide'
+import {cards} from '../data'
+import CatCard from '../components/CatCard'
 
 const Home = () => {
   return (
     <div>
         <Featured/>
         <TrustedBy/>
+        <Slide slidesToShow={5} arrowsScroll={5}>
+          {
+            cards.map((card) => (
+              <CatCard key={card.id} item = {card}/>
+            ))
+          }
+        </Slide>
     </div>
   )
 }
